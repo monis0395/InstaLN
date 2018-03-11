67cc3189cc37ecb7a8dc15d3270b19608b3f5224
monis.q <monis.q@media.net>
2018-03-11 20:51:34 +0530

    added novel views

--- app/src/main/java/com/android/kohaku/instaln/ui/novel/NovelActivity.java
+++ app/src/main/java/com/android/kohaku/instaln/ui/novel/NovelActivity.java
@@ -1,14 +1,23 @@
 package com.android.kohaku.instaln.ui.novel;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.android.kohaku.instaln.R;
 import com.android.kohaku.instaln.data.Model.Chapter;
 import com.android.kohaku.instaln.ui.base.BaseActivity;
+import com.mindorks.placeholderview.annotations.View;
 
 import java.util.List;
 
 public class NovelActivity extends BaseActivity<NovelPresenter> implements NovelContract.View {
+
+
+    @View(R.id.novelName)
+    TextView novelname;
+
+    @View(R.id.summaryDetails)
+    TextView summaryDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
